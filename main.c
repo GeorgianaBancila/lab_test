917c63c39f8189ec8d2ccf0fe8879ee605913722
Georgiana Bancila <bancilageorgiana75@gmail.com>
2023-03-29 23:05:27 +0300

    second commit

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
+#include "stive.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+void verificareparanteze(char paranteze[],Node *stack_top)
+{int i,parantezedeschise=0, parantezeinchise=0;
+ for(i=0;i<strlen(paranteze);i++)
+  {
+      push(&stack_top,i);
+      if (paranteze[i]=='(')
+            parantezedeschise++;
+      else if (paranteze[i]==')')
+        parantezeinchise++;
+
+      if (parantezeinchise>0 && parantezeinchise<=parantezedeschise)
+       {pop(&stack_top), pop(&stack_top);
+       parantezedeschise--;
+       parantezeinchise--;
+  }
+
+  if (parantezeinchise > parantezedeschise)
+    printf("%d ",pop(&stack_top)), parantezeinchise--;
+
+}
+while(!isEmpty(stack_top))
+        printf("%d ",pop(&stack_top));
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    Node *stack_top=NULL;
+ char paranteze[]=")))(";
+
+  verificareparanteze(paranteze,stack_top);
+
     return 0;
 }
---
+++ stive.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+typedef int Data ;
+struct Elem
+{
+Data val;
+struct Elem * next ;
+};
+typedef struct Elem Node ;
+Data top(Node *top);
+void push(Node**top, Data v);
+Data pop(Node**top);
+int isEmpty(Node*top);
+void deleteStack(Node**top);
