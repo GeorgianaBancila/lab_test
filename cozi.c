d771988f30fe6ede523fdb8ddcd926919cf9d8a1
GeorgianaBancila <bancilageorgiana75@gmail.com>
2023-03-30 13:35:57 +0300

    last commit

--- cozi.c
+++ cozi.c
@@ -1,76 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-
 typedef int Data;
 
 struct Elem
 {
-	Data val;
-	struct Elem *next;
+    Data val;
+    struct Elem *next;
 };
 
 typedef struct Elem Node;
 
 struct Q
 {
-	Node *front, *rear;
+    Node *front, *rear;
 };
 typedef struct Q Queue;
 
 Queue *createQueue()
 {
-	Queue *q;
-	q = (Queue *)malloc(sizeof(Queue));
-	if (q == NULL)
-		return NULL;
-	q->front = q->rear = NULL;
-	return q;
+    Queue *q;
+    q = (Queue *)malloc(sizeof(Queue));
+    if (q == NULL)
+        return NULL;
+    q->front = q->rear = NULL;
+    return q;
 }
 
 void enQueue(Queue *q, Data v)
 {
-	Node *newNode = (Node *)malloc(sizeof(Node));
-	newNode->val = v;
-	newNode->next = NULL;
-	if (q->rear == NULL)
-		q->rear = newNode;
-	else
-	{
-		(q->rear)->next = newNode;
-		(q->rear) = newNode;
-	}
-	if (q->front == NULL)
-		q->front = q->rear;
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->val = v;
+    newNode->next = NULL;
+    if (q->rear == NULL)
+        q->front = q->rear = newNode;
+    else
+    {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
 }
 
 Data deQueue(Queue *q)
 {
-	Node *aux;
-	Data d;
-	if (isEmptyQueue(q) == 0)
-		return INT_MIN;
+    Node *aux;
+    Data d;
+    if (isEmptyQueue(q) == 1)
+        return INT_MIN;
 
-	aux = q->front;
-	d = aux->val;
-	q->front = (q->front)->next;
-	free(aux);
-	return d;
+    aux = q->front;
+    d = aux->val;
+    q->front = aux->next;
+    if (q->front == NULL)
+        q->rear = NULL;
+    free(aux);
+    return d;
 }
 
 int isEmptyQueue(Queue *q)
 {
-	return (q->front == NULL);
+    return (q->front == NULL);
 }
 
 void deleteQueue(Queue *q)
 {
-	Node *aux;
-	while (!isEmptyQueue(q))
-	{
-		aux = q->front;
-		q->front = q->front->next;
-		free(aux);
-	}
-	free(q);
+    Node *aux;
+    while (!isEmptyQueue(q))
+    {
+        aux = q->front;
+        q->front = q->front->next;
+        free(aux);
+    }
+    free(q);
 }
--- maincozi.c
+++ maincozi.c
@@ -2,28 +2,31 @@
 #include "stive.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 void reverseQueue(Queue *q) {
     Nod *stackTop = NULL;
-    while(isEmptyQueue(q) != 0) {
+    while(isEmptyQueue(q) == 0) {
         push(&stackTop, deQueue(q));
     }
 
-    while(isEmptyStack(stackTop) != 0) {
+    while(isEmptyStack(stackTop) == 0) {
         enQueue(q, pop(&stackTop));
     }
 }
 
 int main() {
+    int i;
     int elementeCoada[] = {1, 2 ,3 , 4 , 5};
     Queue *q = createQueue();
 
-    for (int i = 0; i < sizeof(elementeCoada) / sizeof(int); i++)  {
+    for ( i = 0; i < sizeof(elementeCoada) / sizeof(int); i++)  {
         enQueue(q, elementeCoada[i]);
     }
     reverseQueue(q);
-    while (isEmptyStack(q) != 0) {
-        printf("%d", deQueue(q));
+    while (isEmptyQueue(q) == 0) {
+        printf("%d ", deQueue(q));
     }
 
+    deleteQueue(q);
+    return 0;
 }
